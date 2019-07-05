a94ca6da0d09cb933a685f3ab00fc3a1395c9b67
kgchicobava <kgchicobava@gmail.com>
2019-07-05 17:00:20 +0300

    Updates

---
+++ precache-manifest.e27a5480bc3db78a0c8332e3563ccf69.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092a53be3f517fae7aa7e1267daece6a",
+    "url": "/calend/index.html"
+  },
+  {
+    "revision": "ce415f1e7fa9d47f0000",
+    "url": "/calend/static/css/main.f3a17a1b.chunk.css"
+  },
+  {
+    "revision": "98cee9ce2f8f2c771641",
+    "url": "/calend/static/js/2.b5eaed63.chunk.js"
+  },
+  {
+    "revision": "ce415f1e7fa9d47f0000",
+    "url": "/calend/static/js/main.48c7c5d4.chunk.js"
+  },
+  {
+    "revision": "a32a888180e8378d943a",
+    "url": "/calend/static/js/runtime~main.309d5b25.js"
+  }
+]);
